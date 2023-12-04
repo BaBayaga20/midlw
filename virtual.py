271fe14a53695245ef8979cdb2dd6b0303a75346
BaBayaga20 <77557837+BaBayaga20@users.noreply.github.com>
2023-12-04 12:57:41 -0600

    Create virtual.py

---
+++ virtual.py
@@ -0,0 +1,87 @@
+import socket
+import threading
+from datetime import datetime, date, time, timezone
+
+IP_MAQ_1 = "192.168.40.128" #"127.0.0.1"
+IP_MAQ_2 = "192.168.40.142"
+IP_MAQ_3 = "192.168.40.136"
+IP_MAQ_4 = "192.168.40.137"
+
+ip_propio = "192.168.40.128" #Infosec2
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 9999
+ADDR = (SERVER, PORT)
+
+def handle_client(client_socket):
+	request = client_socket.recv(1024)
+	now = datetime.now().isoformat()
+#	print(f"Recibido: {request.decode('utf-8')}")
+	rec = request.decode('utf-8') + "\n"
+	with open('Registro','a') as f:
+		f.write(rec)
+	msgreg = "Servidor " + ip_propio + ": Mensaje recibido: "+ request.decode('utf-8') +". Hora: " + now + "\n"
+#client_socket.send(b"Recibido")
+	client_socket.send(msgreg.encode())
+	client_socket.close()
+	with open('Registro','a') as f:
+		f.write(msgreg)
+
+def start_server():
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind(("0.0.0.0",PORT))
+	server.listen(5)
+	with open('Registro','a') as f:
+		f.write("Servidor " + ip_propio + " inicializado.\n")
+#	print("Servidor en espera en el puerto 9999")
+	while True:
+		client_socket, addr = server.accept()
+		now = datetime.now().isoformat()
+		msgreg1 = "Servidor " + ip_propio + ": Conexion realizada con cliente "+ SERVER +". Hora: " + now + "\n"
+		client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+		client_handler.start()
+		with open('Registro','a') as f:
+			f.write(msgreg1)
+
+def send_message(SERVER,msgreg1,message):	
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	now = datetime.now().isoformat()
+	msgclie1 = "Cliente " + ip_propio + ". Realizando conexion con servidor " + SERVER +". Hora: " + now + "\n"
+ #   print(f"Conexion a {SERVER}:{PORT}")
+	with open('Registro','a') as f:
+		f.write(msgclie1)
+		f.write(msgreg1) 
+	client.connect((SERVER,PORT))
+	client.send(message.encode('utf-8'))
+	response = client.recv(1024)
+#    print(f"Server response: {response.decode('utf-8')}")
+	with open('Registro','a') as f:
+		f.write(response.decode('utf-8')) 
+	client.close()
+
+if _name_ == "_main_":
+    # Start the server in a separate thread
+    				
+	server_thread = threading.Thread(target=start_server)
+	server_thread.start()
+
+	while True: 
+		# Simulate client behavior in the main thread
+		opcion = input("Elige a que maquina te quieres conectar: ")
+		match opcion:
+			case "1":
+				SERVER = IP_MAQ_1
+			case "2":
+				SERVER = IP_MAQ_2
+			case "3":
+				SERVER = IP_MAQ_3
+			case "4":	
+				SERVER = IP_MAQ_4
+			case _:
+				SERVER = socket.gethostbyname(socket.gethostname())
+		message = input("Agrega el mensaje: ")
+		now = datetime.now().isoformat()
+		msgreg1 = "Cliente " + ip_propio + ": Enviado a " + SERVER + ": " + message  +  ". Hora: " + now + "\n"
+		send_message(SERVER,msgreg1,message)
+
+	# Wait for the server thread to finish before exiting
+	server_thread.join()
